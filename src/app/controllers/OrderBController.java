c2eed6132efff416d062dcb6362521b302cdd2b3
Ann Kruvoshei <coolskeletonpapyrus37@gmail.com>
2023-01-13 08:59:26 +0300

    Initial commit

---
+++ src/app/controllers/OrderBController.java
@@ -0,0 +1,51 @@
+package app.controllers;
+
+import app.base.BaseController;
+import app.models.OrderBModel;
+import app.utils.Constants;
+import app.utils.Rounder;
+import app.views.OrderView;
+
+public class OrderBController implements BaseController {
+
+    private OrderBModel model;
+    private String[] data;
+    private String orderCostRounded;
+    private String deliveryCostRounded;
+    private double orderCost;
+
+    public void handleData() {
+
+        OrderView view = new OrderView();
+        data = getData();
+        model = new OrderBModel();
+
+        double orderCost = getOrderCost();
+        orderCostRounded = Rounder.roundValue(orderCost);
+
+        double deliveryCost = getDeliveryCost();
+        deliveryCostRounded = Rounder.roundValue(deliveryCost);
+
+        view.getOutput(formOutput());
+    }
+
+    private String[] getData() {
+        return data;
+    }
+
+    private double getOrderCost() {
+        return model.getOrderCost(Integer.parseInt(data[1]),
+                Double.parseDouble(data[3]));
+    }
+
+    private double getDeliveryCost() {
+        return  model.getDeliveryCost(Integer.parseInt(data[1]),
+                Double.parseDouble(data[2]));
+    }
+
+    private String formOutput() {
+        return "\nOrder " + data[0] + " cost is " + orderCostRounded +
+                " " + orderCost + "\nDelivery cost is " + Constants.CURRENCY +
+                " " + deliveryCostRounded;
+    }
+}
